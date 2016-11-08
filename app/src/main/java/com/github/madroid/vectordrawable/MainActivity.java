8a0da84a8320863f6eaaadeb553162b70e50fcff
madroid <madroidmaq@gmail.com>
2016-11-08 20:51:02 +0800

    use support library to get vector drawable

--- app/src/main/java/com/github/madroid/vectordrawable/MainActivity.java
+++ app/src/main/java/com/github/madroid/vectordrawable/MainActivity.java
@@ -1,13 +1,24 @@
 package com.github.madroid.vectordrawable;
 
+import android.graphics.drawable.Drawable;
+import android.support.graphics.drawable.VectorDrawableCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
 
 public class MainActivity extends AppCompatActivity {
+
+    static {
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Drawable drawable = VectorDrawableCompat.create(getResources(), R.drawable.ic_android_black_24dp, getTheme());
+        findViewById(R.id.view).setBackground(drawable);
+
     }
 }
